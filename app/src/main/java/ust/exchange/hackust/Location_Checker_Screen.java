09e2ffbd58cb3c00e3c7939db7d87a39813052d8
bigjohnlin <bigjohnlin@gmail.com>
2017-04-22 22:47:17 +0800

    button to next map

--- app/src/main/java/ust/exchange/hackust/Location_Checker_Screen.java
+++ app/src/main/java/ust/exchange/hackust/Location_Checker_Screen.java
@@ -1,13 +1,20 @@
 package ust.exchange.hackust;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Location_Checker_Screen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location__checker__screen);
+    }
+
+    public void tempMethod(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
