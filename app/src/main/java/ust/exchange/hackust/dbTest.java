7251a9fe2e3511f12f5b2543afce8ab47fa60b3b
saejin9 <saejin9>
2017-04-23 04:12:23 +0900

    basic setting for using firebase

--- app/src/main/java/ust/exchange/hackust/dbTest.java
+++ app/src/main/java/ust/exchange/hackust/dbTest.java
@@ -1,72 +1,76 @@
 package ust.exchange.hackust;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class dbTest extends AppCompatActivity {
+    public class myObject {
+        private float begintrip_lat;
+        private float begintrip_lng;
+        private String begintrip_timestamp_local;
+        private float dropoff_lat;
+        private float dropoff_lng;
+        private String is_completed;
+        private float original_fare_local;
+        private int surge_multiplier;
+        private int toll_amount_local;
+        private float trip_distance_miles;
+        private int weekofday;
 
-    private static final String TAG = "MainActivity";
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-
-    TextView tvMessage;
-    EditText etNewMessage;
-    Button btUpdate;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_db_test);
-
-        tvMessage = (TextView) findViewById(R.id.tv_message);
-        etNewMessage = (EditText) findViewById(R.id.et_newData);
-        btUpdate = (Button) findViewById(R.id.bt_update);
-
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
-
-        //버튼 이벤트
-        btUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String newMessage = etNewMessage.getText().toString();
-                myRef.setValue(newMessage);
-            }
-        });
-
-        // Read from the database
-        // 그리고 데이터베이스에 변경사항이 있으면 실행된다.
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                //데이터를 화면에 출력해 준다.
-                tvMessage.setText(value);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        public myObject() {}
     }
+
+    DatabaseReference ref = FirebaseDatabase.getInstance()
+            .getReferenceFromUrl("https://hackust-75a8f.firebaseio.com/");
+
+
+
+    ValueEventListener objectListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // Get Post object and use the values to update the UI
+            myObject object = dataSnapshot.getValue(myObject.class);
+            // ...
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            // Getting Post failed, log a message
+
+            // ...
+        }
+    };
+
+    private TextView textViewList;
+    //FirebaseDatabase.set
+
+
 }
+
+
+
+
 
