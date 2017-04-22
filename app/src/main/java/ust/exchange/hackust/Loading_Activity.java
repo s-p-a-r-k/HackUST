8e43217e24afe7307fb57c1d382048d3a1a8ca14
bigjohnlin <bigjohnlin@gmail.com>
2017-04-22 20:26:58 +0800

    loading screen animation complete

--- app/src/main/java/ust/exchange/hackust/Loading_Activity.java
+++ app/src/main/java/ust/exchange/hackust/Loading_Activity.java
@@ -1,6 +1,7 @@
 package ust.exchange.hackust;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +9,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 
+import com.wang.avi.AVLoadingIndicatorView;
+import com.wang.avi.indicators.BallTrianglePathIndicator;
+
 public class Loading_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_);
+        new CountDownTimer(5000, 1000) {
 
+            public void onTick(long millisUntilFinished) {
+                startAnim();
+            }
+
+            public void onFinish() {
+                endAnim();
+            }
+        }.start();
+    }
+    void startAnim() {
+        AVLoadingIndicatorView avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
+        avi.show();
     }
 
+    void endAnim() {
+        AVLoadingIndicatorView avi = (AVLoadingIndicatorView) findViewById(R.id.avi);
+        avi.hide();
+    }
 }
